191ef4f1ccada97c298b72f9ce08e4cf9e8e832d
Loecki03 <luis.loeck@myhsba.de>
2024-10-03 18:51:09 +0200

    add return to homepage after adding court

--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -67,6 +67,15 @@ class _HomePageState extends State<HomePage> {
     _trackLocationChanges();
   }
 
+  // This method will be called when returning to HomePage from AddCourtPage
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Trigger user location when the user returns to HomePage
+    _getUserLocation();
+  }
+
   @override
   void dispose() {
     _positionStream?.cancel();

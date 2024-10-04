bbc4a17462751659ff62ef1d112a6106db4218d7
Christian <christianpaul.wiener@gmail.com>
2024-10-04 21:02:56 +0200

    update account page with recentley played section

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -26,6 +26,12 @@ class _AccountPageState extends State<AccountPage> {
   final List<String> positions = ['PG', 'SG', 'SF', 'PF', 'C'];
   final List<String> skillLevels = ['Beginner', 'Intermediate', 'Advanced', 'Expert', 'Pro']; // Updated skill levels
 
+  // Variables to store recently played courts
+  String? firstCourtImageUrl;
+  String? secondCourtImageUrl;
+  String? firstCourtName;
+  String? secondCourtName;
+
   @override
   void initState() {
     super.initState();
@@ -44,8 +50,14 @@ class _AccountPageState extends State<AccountPage> {
             cityController.text = data['city'] ?? '';
             heightController.text = data['height']?.toString() ?? '';
             selectedPosition = positions.contains(data['position']) ? data['position'] : null;
-            selectedSkillLevel = skillLevels.contains(data['skillLevel']) ? data['skillLevel'] : null; // Skill level
+            selectedSkillLevel = skillLevels.contains(data['skillLevel']) ? data['skillLevel'] : null;
             _profileImageUrl = data['profileImage'] ?? '';
+
+            // Fetch the last two played courts
+            if (data['last_courts'] != null && data['last_courts'].length > 0) {
+              List<dynamic> lastCourts = data['last_courts'];
+              _fetchCourtDetails(lastCourts); // Fetch last two courts
+            }
           });
         }
       } catch (e) {
@@ -53,6 +65,54 @@ class _AccountPageState extends State<AccountPage> {
           SnackBar(content: Text("Failed to load profile: $e")),
         );
       }
+    }
+  }
+
+  Future<void> _fetchCourtDetails(List<dynamic> lastCourts) async {
+    try {
+      if (lastCourts.isNotEmpty) {
+        // First court
+        if (lastCourts.length > 0) {
+          var firstCourtId = lastCourts[lastCourts.length - 1]['placeId'];
+          var firstCourtDoc = await FirebaseFirestore.instance.collection('basketball_courts').doc(firstCourtId).get();
+          if (firstCourtDoc.exists) {
+            var courtData = firstCourtDoc.data() as Map<String, dynamic>;
+
+            setState(() {
+              firstCourtImageUrl = (courtData['imageUrls'] != null && courtData['imageUrls'].isNotEmpty)
+                  ? courtData['imageUrls'][0]
+                  : null;
+              firstCourtName = courtData['name'];
+
+              print("First Court PlaceId: $firstCourtId");
+              print("First Court Image URL: $firstCourtImageUrl");
+            });
+          }
+        }
+
+        // Second court
+        if (lastCourts.length > 1) {
+          var secondCourtId = lastCourts[lastCourts.length - 2]['placeId'];
+          var secondCourtDoc = await FirebaseFirestore.instance.collection('basketball_courts').doc(secondCourtId).get();
+          if (secondCourtDoc.exists) {
+            var courtData = secondCourtDoc.data() as Map<String, dynamic>;
+
+            setState(() {
+              secondCourtImageUrl = (courtData['imageUrls'] != null && courtData['imageUrls'].isNotEmpty)
+                  ? courtData['imageUrls'][0]
+                  : null;
+              secondCourtName = courtData['name'];
+
+              print("Second Court PlaceId: $secondCourtId");
+              print("Second Court Image URL: $secondCourtImageUrl");
+            });
+          }
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Failed to load court details: $e")),
+      );
     }
   }
 
@@ -78,7 +138,7 @@ class _AccountPageState extends State<AccountPage> {
           'city': cityController.text,
           'height': height,
           'position': selectedPosition ?? positions[0],
-          'skillLevel': selectedSkillLevel ?? skillLevels[0], // Save the selected skill level
+          'skillLevel': selectedSkillLevel ?? skillLevels[0],
           'profileImage': _profileImageUrl ?? '',
         }, SetOptions(merge: true));
         ScaffoldMessenger.of(context).showSnackBar(
@@ -151,7 +211,7 @@ class _AccountPageState extends State<AccountPage> {
         ],
       ),
       body: Container(
-        color: Colors.grey[850],  // Ensure background covers entire screen
+        color: Colors.grey[850],
         child: Column(
           children: [
             Expanded(
@@ -164,13 +224,13 @@ class _AccountPageState extends State<AccountPage> {
                         child: Stack(
                           children: [
                             CircleAvatar(
-                              radius: 100,  // Profile picture size
+                              radius: 100,
                               backgroundColor: Colors.grey[400],
                               backgroundImage: _profileImageUrl != null && _profileImageUrl!.isNotEmpty
-                                  ? NetworkImage(_profileImageUrl!)  // Load image from Firestore URL
+                                  ? NetworkImage(_profileImageUrl!)
                                   : null,
                               child: _profileImageUrl == null || _profileImageUrl!.isEmpty
-                                  ? Icon(Icons.person, size: 100, color: Colors.white)  // Default icon if no image
+                                  ? Icon(Icons.person, size: 100, color: Colors.white)
                                   : null,
                             ),
                             Positioned(
@@ -182,7 +242,7 @@ class _AccountPageState extends State<AccountPage> {
                                 },
                                 child: CircleAvatar(
                                   backgroundColor: Colors.teal,
-                                  radius: 28,  // Camera icon size
+                                  radius: 28,
                                   child: Icon(Icons.camera_alt, color: Colors.white, size: 28),
                                 ),
                               ),
@@ -204,7 +264,7 @@ class _AccountPageState extends State<AccountPage> {
                                   SizedBox(height: 20),
                                   _buildTextField(label: 'City', controller: cityController),
                                   SizedBox(height: 20),
-                                  Expanded(  // Wrap the Position Dropdown inside Expanded
+                                  Expanded(
                                     child: _buildDropdownField(
                                       label: 'Position',
                                       items: positions,
@@ -231,7 +291,7 @@ class _AccountPageState extends State<AccountPage> {
                                   SizedBox(height: 20),
                                   _buildTextField(label: 'Height (cm)', controller: heightController, keyboardType: TextInputType.number),
                                   SizedBox(height: 20),
-                                  Expanded(  // Wrap the Skill Level Dropdown inside Expanded
+                                  Expanded(
                                     child: _buildDropdownField(
                                       label: 'Skill Level',
                                       items: skillLevels,
@@ -241,7 +301,7 @@ class _AccountPageState extends State<AccountPage> {
                                           selectedSkillLevel = newValue;
                                         });
                                       },
-                                      isExpanded: true,  // Ensure it uses full width
+                                      isExpanded: true,
                                     ),
                                   ),
                                 ],
@@ -253,7 +313,7 @@ class _AccountPageState extends State<AccountPage> {
 
                       SizedBox(height: 40),
 
-                      // Recently Played Section
+// Recently Played Section
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.center, // Center align the heading
                         children: [
@@ -267,23 +327,90 @@ class _AccountPageState extends State<AccountPage> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              // Placeholder for the first court
-                              Container(
-                                width: 150,
-                                height: 150,
-                                color: Colors.grey[700],
-                                child: Center(
-                                  child: Icon(Icons.location_on, color: Colors.white, size: 50),
-                                ),
+                              // First Court Image with Name below
+                              Column(
+                                children: [
+                                  Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[700],
+                                      border: Border.all(color: Colors.grey[800]!), // Optional border for consistency
+                                    ),
+                                    child: firstCourtImageUrl != null
+                                        ? ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0), // Optional: Add rounded corners
+                                      child: Image.network(
+                                        firstCourtImageUrl!,
+                                        fit: BoxFit.cover, // Cover ensures the image fills the container
+                                        width: 150,
+                                        height: 150,
+                                      ),
+                                    )
+                                        : Center(
+                                      child: Icon(Icons.location_on, color: Colors.white, size: 50),
+                                    ),
+                                  ),
+                                  SizedBox(height: 8), // Space between image and name
+                                  if (firstCourtName != null)
+                                    Container(
+                                      width: 150, // Ensure the width is the same as the image
+                                      child: Text(
+                                        firstCourtName!,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                        maxLines: 2, // Limit the text to 2 lines
+                                        overflow: TextOverflow.ellipsis, // Add ellipsis if text overflows
+                                      ),
+                                    ),
+                                ],
                               ),
-                              // Placeholder for the second court
-                              Container(
-                                width: 150,
-                                height: 150,
-                                color: Colors.grey[700],
-                                child: Center(
-                                  child: Icon(Icons.location_on, color: Colors.white, size: 50),
-                                ),
+
+                              // Second Court Image with Name below
+                              Column(
+                                children: [
+                                  Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[700],
+                                      border: Border.all(color: Colors.grey[800]!),
+                                    ),
+                                    child: secondCourtImageUrl != null
+                                        ? ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      child: Image.network(
+                                        secondCourtImageUrl!,
+                                        fit: BoxFit.cover,
+                                        width: 150,
+                                        height: 150,
+                                      ),
+                                    )
+                                        : Center(
+                                      child: Icon(Icons.location_on, color: Colors.white, size: 50),
+                                    ),
+                                  ),
+                                  SizedBox(height: 8), // Space between image and name
+                                  if (secondCourtName != null)
+                                    Container(
+                                      width: 150, // Ensure the width is the same as the image
+                                      child: Text(
+                                        secondCourtName!,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                ],
                               ),
                             ],
                           ),
@@ -343,7 +470,7 @@ class _AccountPageState extends State<AccountPage> {
           SizedBox(height: 12),
           DropdownButtonFormField<String>(
             value: value,
-            isExpanded: isExpanded,  // Allow the dropdown to take full width
+            isExpanded: isExpanded,
             dropdownColor: Colors.grey[850],
             icon: Icon(Icons.arrow_drop_down, color: Colors.white),
             decoration: InputDecoration(
@@ -397,6 +524,8 @@ class _AccountPageState extends State<AccountPage> {
         });
   }
 }
+
+
 
 
 
